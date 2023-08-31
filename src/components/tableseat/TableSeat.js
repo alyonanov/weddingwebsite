8ca814849d3b1df645fdae2269c98ea3639df45d
alyonanov <unixcats@gmail.com>
2023-08-31 20:53:36 +0300

    add plate

--- src/components/tableseat/TableSeat.js
+++ src/components/tableseat/TableSeat.js
@@ -1,8 +1,9 @@
 import './TableSeat.css'
 import { ChairSVG } from '../../resources/ChairSVG';
+import { PlateSVG } from '../../resources/PlateSVG';
 
 const TableSeat =() => {
-    const id = 6
+    const id = 77;
     
     return (
         <div className='tableBlock' id="table">
@@ -11,6 +12,9 @@ const TableSeat =() => {
                     {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map(el =>(
                         <div className='chairDiv'>
                             <ChairSVG isActive={el === id}/>
+                            <div className='plateDiv'>
+                                <PlateSVG isActive={el === id}/>
+                            </div>
                         </div> ))}
                 </div>
                 <div className='tableArr'>
@@ -35,6 +39,9 @@ const TableSeat =() => {
                     {[19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36].map(el =>(
                         <div className='chairDiv'>
                             <ChairSVG isActive={el === id}/>
+                            <div className='plateDiv'>
+                                <PlateSVG isActive={el === id}/>
+                            </div>
                         </div> ))}
                     </div>
             </div>
@@ -43,6 +50,9 @@ const TableSeat =() => {
                     {[37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56].map(el =>(
                         <div className='chairDiv'>
                             <ChairSVG isActive={el === id}/>
+                            <div className='plateDiv'>
+                                <PlateSVG isActive={el === id}/>
+                            </div>
                         </div> ))}
                     </div>
                 <div className='tableArr'>
@@ -66,11 +76,17 @@ const TableSeat =() => {
                     {[57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76].map(el =>(
                         <div className='chairDiv'>
                             <ChairSVG isActive={el === id}/>
+                            <div className='plateDiv'>
+                                <PlateSVG isActive={el === id}/>
+                            </div>
                         </div> ))}
                     
                     {[77].map(el =>(
                         <div className='chairDiv' id='lastChair'>
                             <ChairSVG isActive={el === id}/>
+                            <div className='plateDiv'>
+                                <PlateSVG isActive={el === id}/>
+                            </div>
                         </div> ))}
                 </div>
             </div>

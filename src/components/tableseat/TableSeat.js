0c1fd31c82e05e0468418caf4db0709be2786033
alyonanov <unixcats@gmail.com>
2023-08-31 18:25:37 +0300

    change tables

--- src/components/tableseat/TableSeat.js
+++ src/components/tableseat/TableSeat.js
@@ -1,118 +1,78 @@
 import './TableSeat.css'
+import { ChairSVG } from '../../resources/ChairSVG';
 
 const TableSeat =() => {
+    const id = 6
     
     return (
         <div className='tableBlock' id="table">
             <div className='firstTable'>
                 <div className='chairArrLeft'>
-                    {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(el =>(
+                    {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map(el =>(
                         <div className='chairDiv'>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="chair"
-                                viewBox="0 0 29.43 55.35"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={0.6}
-                                >
-                                <path
-                                d="M7.4,10.7s5-.86,7.11,1.73,9.94,12.51,1.42,32.35c-4.26,5.61-10.66,2.16-10.66,2.16Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                                <polygon class="cls-1" points="14.73 7.83 9.75 10.85 11.1 12.64 16.86 10.85 14.73 7.83"/>
-                                <polygon class="cls-1" points="14.13 19.25 21.12 17.76 21.83 19.91 14.57 21.09 14.13 19.25"/>
-                                <polygon class="cls-1" points="14.73 27.25 23.25 26.38 22.54 28.97 14.73 28.97 14.73 27.25"/>
-                                <polygon class="cls-1" points="13.97 34.97 21.83 35.44 21.12 38.03 12.6 36.31 13.97 34.97"/>
-                                <polygon class="cls-1" points="11.98 41.24 16.86 43.64 14.73 46.23 10.46 42.35 11.98 41.24"/>
-                                <path class="cls-1" d="M15.93,3.37,13.8,2.07s-2.84,0-2.13,1.3,7.1,6,7.1,6,5.68,4.31,7.1,9.92,6.4,18.12-11.36,32.36c-2.84,1.72-2.84,3.45-2.84,3.45s.71,2.59,5,.43,16.34-16,17-25C33.69,24.94,29.43,11.13,15.93,3.37Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                            </svg>
+                            <ChairSVG isActive={el === id}/>
                         </div> ))}
+                </div>
+                <div className='tableArr'>
+                    <div className='table' id='first'>
+                        <div className='tableBoard'>
+                            <p>1</p>
+                        </div>
                     </div>
-                    <div className='table'></div>
+                    <div className='table' id='second'>
+                        <div className='tableBoard'>
+                            <p>2</p>
+                        </div>
+                    </div>
+                    <div className='table' id='third'>
+                        <div className='tableBoard'>
+                            <p>3</p>
+                        </div>
+                    </div>
+                </div>
+                
                 <div className='chairArrRight'>
-                    {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(el =>(
+                    {[19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36].map(el =>(
                         <div className='chairDiv'>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="chair"
-                                viewBox="0 0 29.43 55.35"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={0.6}
-                                >
-                                <path
-                                d="M7.4,10.7s5-.86,7.11,1.73,9.94,12.51,1.42,32.35c-4.26,5.61-10.66,2.16-10.66,2.16Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                                <polygon class="cls-1" points="14.73 7.83 9.75 10.85 11.1 12.64 16.86 10.85 14.73 7.83"/>
-                                <polygon class="cls-1" points="14.13 19.25 21.12 17.76 21.83 19.91 14.57 21.09 14.13 19.25"/>
-                                <polygon class="cls-1" points="14.73 27.25 23.25 26.38 22.54 28.97 14.73 28.97 14.73 27.25"/>
-                                <polygon class="cls-1" points="13.97 34.97 21.83 35.44 21.12 38.03 12.6 36.31 13.97 34.97"/>
-                                <polygon class="cls-1" points="11.98 41.24 16.86 43.64 14.73 46.23 10.46 42.35 11.98 41.24"/>
-                                <path class="cls-1" d="M15.93,3.37,13.8,2.07s-2.84,0-2.13,1.3,7.1,6,7.1,6,5.68,4.31,7.1,9.92,6.4,18.12-11.36,32.36c-2.84,1.72-2.84,3.45-2.84,3.45s.71,2.59,5,.43,16.34-16,17-25C33.69,24.94,29.43,11.13,15.93,3.37Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                            </svg>
+                            <ChairSVG isActive={el === id}/>
                         </div> ))}
                     </div>
             </div>
             <div className='secondTable'>
                 <div className='chairArrLeft'>
-                    {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(el =>(
+                    {[37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56].map(el =>(
                         <div className='chairDiv'>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="chair"
-                                viewBox="0 0 29.43 55.35"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={0.6}
-                                >
-                                <path
-                                d="M7.4,10.7s5-.86,7.11,1.73,9.94,12.51,1.42,32.35c-4.26,5.61-10.66,2.16-10.66,2.16Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                                <polygon class="cls-1" points="14.73 7.83 9.75 10.85 11.1 12.64 16.86 10.85 14.73 7.83"/>
-                                <polygon class="cls-1" points="14.13 19.25 21.12 17.76 21.83 19.91 14.57 21.09 14.13 19.25"/>
-                                <polygon class="cls-1" points="14.73 27.25 23.25 26.38 22.54 28.97 14.73 28.97 14.73 27.25"/>
-                                <polygon class="cls-1" points="13.97 34.97 21.83 35.44 21.12 38.03 12.6 36.31 13.97 34.97"/>
-                                <polygon class="cls-1" points="11.98 41.24 16.86 43.64 14.73 46.23 10.46 42.35 11.98 41.24"/>
-                                <path class="cls-1" d="M15.93,3.37,13.8,2.07s-2.84,0-2.13,1.3,7.1,6,7.1,6,5.68,4.31,7.1,9.92,6.4,18.12-11.36,32.36c-2.84,1.72-2.84,3.45-2.84,3.45s.71,2.59,5,.43,16.34-16,17-25C33.69,24.94,29.43,11.13,15.93,3.37Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                            </svg>
+                            <ChairSVG isActive={el === id}/>
                         </div> ))}
                     </div>
-                    <div className='table'></div>
+                <div className='tableArr'>
+                    <div className='table' id='fourth'>
+                        <div className='tableBoard'>
+                            <p>4</p>
+                        </div>
+                    </div>
+                    <div className='table' id='fifth'>
+                        <div className='tableBoard'>
+                            <p>5</p>
+                        </div>
+                    </div>
+                    <div className='table' id='sixth'>
+                        <div className='tableBoard'>
+                            <p>6</p>
+                        </div>
+                    </div>
+                </div>
                 <div className='chairArrRight'>
-                    {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(el =>(
+                    {[57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76].map(el =>(
                         <div className='chairDiv'>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="chair"
-                                viewBox="0 0 29.43 55.35"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={0.6}
-                                >
-                                <path
-                                d="M7.4,10.7s5-.86,7.11,1.73,9.94,12.51,1.42,32.35c-4.26,5.61-10.66,2.16-10.66,2.16Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                                <polygon class="cls-1" points="14.73 7.83 9.75 10.85 11.1 12.64 16.86 10.85 14.73 7.83"/>
-                                <polygon class="cls-1" points="14.13 19.25 21.12 17.76 21.83 19.91 14.57 21.09 14.13 19.25"/>
-                                <polygon class="cls-1" points="14.73 27.25 23.25 26.38 22.54 28.97 14.73 28.97 14.73 27.25"/>
-                                <polygon class="cls-1" points="13.97 34.97 21.83 35.44 21.12 38.03 12.6 36.31 13.97 34.97"/>
-                                <polygon class="cls-1" points="11.98 41.24 16.86 43.64 14.73 46.23 10.46 42.35 11.98 41.24"/>
-                                <path class="cls-1" d="M15.93,3.37,13.8,2.07s-2.84,0-2.13,1.3,7.1,6,7.1,6,5.68,4.31,7.1,9.92,6.4,18.12-11.36,32.36c-2.84,1.72-2.84,3.45-2.84,3.45s.71,2.59,5,.43,16.34-16,17-25C33.69,24.94,29.43,11.13,15.93,3.37Z" 
-                                transform="translate(-4.75 -1.57)"
-                                />
-                            </svg>
+                            <ChairSVG isActive={el === id}/>
                         </div> ))}
-                    </div>
+                    
+                    {[77].map(el =>(
+                        <div className='chairDiv' id='lastChair'>
+                            <ChairSVG isActive={el === id}/>
+                        </div> ))}
+                </div>
             </div>
            
             
